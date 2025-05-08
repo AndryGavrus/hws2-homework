4f7722e5ec70cb6bf1ee0744600c13d2ca0de87a
Andrey <andry.gavrus@gmail.com>
2025-05-08 21:46:17 +0300

    solve 11 task

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,34 +1,32 @@
-import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import React from "react"
+import { Slider, SliderProps } from "@mui/material"
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                // color: '#52af77', // Slider color
-                // height: 8, // Height of the slider
-                // '& .MuiSlider-thumb': {
-                //     height: 24, // Thumb height
-                //     width: 24, // Thumb width
-                //     backgroundColor: 'white', // Thumb background color
-                //     border: '2px solid currentColor', // Thumb border
-                //     '&:hover': {
-                //         boxShadow: 'inherit', // Shadow on hover
-                //     },
-                //     '&.Mui-active': {
-                //         boxShadow: 'inherit', // Shadow when active
-                //     },
-                // },
-                // '& .MuiSlider-track': {
-                //     height: 8, // Track height
-                //     borderRadius: 4, // Track border radius
-                // },
-                // '& .MuiSlider-rail': {
-                //     height: 8, // Rail height
-                //     borderRadius: 4, // Rail border radius
-                // },
+            sx={{
+                color: "#1976d2",
+                height: 8,
+ // Устанавливаем ширину слайдера на 100% от родительского контейнера
+                "& .MuiSlider-thumb": {
+                    height: 24,
+                    width: 24,
+                    backgroundColor: "#fff",
+                    border: "2px solid currentColor",
+                    "&:hover": {
+                        boxShadow: "inherit",
+                    },
+                },
+                "& .MuiSlider-track": {
+                    height: 8,
+                    borderRadius: 4,
+                },
+                "& .MuiSlider-rail": {
+                    height: 8,
+                    borderRadius: 4,
+                },
             }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+            {...props} // Передаем все пропсы слайдеру
         />
     )
 }
